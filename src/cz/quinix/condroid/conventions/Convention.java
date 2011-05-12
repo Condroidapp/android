e8d760fe1ad2b7c27e7bffb4df04baa8872bb0fd
Quinix <quinix@quinix.cz>
2011-05-12 21:38:21 +0200

    Convention activity - now shows icon and two lines list

--- src/cz/quinix/condroid/conventions/Convention.java
+++ src/cz/quinix/condroid/conventions/Convention.java
@@ -1,8 +1,34 @@
 package cz.quinix.condroid.conventions;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.graphics.drawable.Drawable;
+
 public class Convention {
 	public String name = "";
 	public String iconUrl = "";
 	public String date = "";
-	public int cid = 0;	
+	public int cid = 0;
+	public Drawable getImage() {
+		Drawable d = null;
+		if(iconUrl != null) {
+			InputStream io;
+			try {
+				io = (InputStream) new URL(this.iconUrl).getContent();
+				d = Drawable.createFromStream(io, "x");
+				
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		return d;
+	}	
 }
--- src/cz/quinix/condroid/conventions/ConventionsActivity.java
+++ src/cz/quinix/condroid/conventions/ConventionsActivity.java
@@ -9,60 +9,60 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Xml;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.TwoLineListItem;
 import cz.quinix.condroid.R;
 
 public class ConventionsActivity extends ListActivity {
 	/** Called when the activity is first created. */
 	private static String list_url = "http://condroid.quinix.cz/api/con-list";
-	static final String[] COUNTRIES = new String[] { "Afghanistan", "Albania" };
-
+	
+	private static Convention[] cons;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		List<Convention> l = this.loadCons();
-		String[] cons = new String[l.size()];
-		Convention c = null; 
-		for(int i=0; i<l.size(); i++) {
-			c = l.get(i);
-			cons[i] = c.name;
-		}
-		this.setListAdapter(new ArrayAdapter<String>(this, R.layout.cons_list,
-				cons));
+		
+
+		this.setListAdapter(new ConventionListAdapter(this, R.layout.cons_list, this.loadCons()));
 
 	}
 
-	private List<Convention> loadCons() {
+	private Convention[] loadCons() {
+		if(ConventionsActivity.cons != null) {
+			return ConventionsActivity.cons;
+		}
 		List<Convention> messages = null;
 		XmlPullParser pull = Xml.newPullParser();
 		Convention con = null;
-		
+
 		try {
 			URL url = new URL(ConventionsActivity.list_url);
 			URLConnection conn = url.openConnection();
-			
-			
 
-			
-			
 			pull.setInput(conn.getInputStream(), null);
 		} catch (Exception ex) {
 			Toast.makeText(this, "Can't load conventions list.",
 					Toast.LENGTH_LONG).show();
 
 		}
-			int eventType = 0;
-			try {
-				eventType = pull.getEventType();
-			} catch (XmlPullParserException e) {
-				Toast.makeText(this, "XML error - 1.",
-						Toast.LENGTH_LONG).show();
-				e.printStackTrace();
-			}
-			try {
+		int eventType = 0;
+		try {
+			eventType = pull.getEventType();
+		} catch (XmlPullParserException e) {
+			Toast.makeText(this, "XML error - 1.", Toast.LENGTH_LONG).show();
+			e.printStackTrace();
+		}
+		try {
 			while (eventType != XmlPullParser.END_DOCUMENT) {
 				switch (eventType) {
 				case XmlPullParser.START_DOCUMENT:
@@ -93,24 +93,66 @@ public class ConventionsActivity extends ListActivity {
 					name = pull.getName();
 					if (name.equalsIgnoreCase("convention") && con != null) {
 						messages.add(con);
-					} 
+					}
 					break;
 				default:
 					break;
 				}
 				eventType = pull.next();
 			}
-			} catch (Exception e) {
-				Toast.makeText(this, "XML error - 2.",
-						Toast.LENGTH_LONG).show();
-				e.printStackTrace();
-			}
-			
-
-		
-
-		return messages;
+		} catch (Exception e) {
+			Toast.makeText(this, "XML error - 2.", Toast.LENGTH_LONG).show();
+			e.printStackTrace();
+		}
+		Convention[] c = new Convention[messages.size()];
+		for (int i = 0; i < messages.size(); i++) {
+			c[i] = messages.get(i);
+		}
+		return c;
 	}
 
-	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+
+	private class ConventionListAdapter extends ArrayAdapter<Convention> {
+
+		private Convention[] items;
+
+		public ConventionListAdapter(Context context, int textViewResourceId,
+				Convention[] items) {
+			super(context, textViewResourceId, items);
+			this.items = items;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if (v == null) {
+				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = vi.inflate(R.layout.convention_list_item, null);
+			}
+
+			Convention it = items[position];
+			if (it != null) {
+				ImageView iv = (ImageView) v.findViewById(R.id.convention_list_item_image);
+				if (iv != null) {
+					iv.setImageDrawable(it.getImage());
+				}
+				TextView tw = (TextView) v.findViewById(R.id.convention_list_item_text);
+				if (tw != null) {
+					tw.setText(it.name);
+				}
+				TextView tw2 = (TextView) v.findViewById(R.id.convention_list_item_date);
+				if (tw2 != null) {
+					tw2.setText(it.date);
+				}
+			}
+
+			return v;
+		}
+
+	}
 }
