43973ba809295383f02188420356c49e2043cb7d
Quinix <quinix@quinix.cz>
2011-05-12 21:45:09 +0200

    layout changes

--- src/cz/quinix/condroid/conventions/ConventionsActivity.java
+++ src/cz/quinix/condroid/conventions/ConventionsActivity.java
@@ -11,16 +11,17 @@ import org.xmlpull.v1.XmlPullParserException;
 import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.TwoLineListItem;
 import cz.quinix.condroid.R;
 
 public class ConventionsActivity extends ListActivity {
