8afdfd10382f6fb68cfd81e0ac66dfa7119007f7
Quinix <quinix@quinix.cz>
2012-06-17 21:58:00 +0200

    Changed share program text [closes #32]

--- src/cz/quinix/condroid/ui/listeners/ShareProgramListener.java
+++ src/cz/quinix/condroid/ui/listeners/ShareProgramListener.java
@@ -6,6 +6,10 @@ import android.view.View;
 import cz.quinix.condroid.model.Annotation;
 import cz.quinix.condroid.ui.ShowAnnotation;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class ShareProgramListener implements View.OnClickListener {
 
     Activity activity;
@@ -22,11 +26,23 @@ public class ShareProgramListener implements View.OnClickListener {
     }
 
     public void invoke(Annotation annotation) {
+        activity.startActivity(Intent.createChooser(this.getShareActionIntent(annotation), "Sdílet"));
+    }
+
+    public Intent getShareActionIntent(Annotation annotation) {
+        DateFormat dayFormat = new SimpleDateFormat(
+                "EE dd.MM. HH:mm", new Locale("cs", "CZ"));
+        DateFormat hourFormat = new SimpleDateFormat(
+                "HH:mm", new Locale("cs", "CZ"));
+
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, annotation.getTitle() + ", @" + annotation.getLocation() + "");
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Právě jsem na pořadu ");
-        activity.startActivity(Intent.createChooser(intent, "Sdílet"));
+        intent.putExtra(Intent.EXTRA_TEXT, annotation.getTitle() +
+                ((annotation.getLocation() != null && !annotation.getLocation().equals("")) ? ", @" + annotation.getLocation() : "") +
+                ((annotation.getStartTime() != null && annotation.getEndTime() != null) ? ", " + dayFormat.format(annotation.getStartTime()) + " - " + hourFormat.format(annotation.getEndTime()) : "")
+        );
+        //intent.putExtra(Intent.EXTRA_SUBJECT, "Právě jsem na pořadu ");
+        return intent;
     }
 
 }
