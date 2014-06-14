1355f547e6516d2bcc3335565798f724830a800b
Jan Langer <jan.langer@gmail.com>
2014-06-14 21:32:23 +0200

    added donate implementation

--- Condroid/src/main/java/cz/quinix/condroid/ui/AboutDialog.java
+++ Condroid/src/main/java/cz/quinix/condroid/ui/AboutDialog.java
@@ -25,6 +25,7 @@ import cz.quinix.condroid.util.Purchase;
 
 public class AboutDialog {
 
+    private AlertDialog dialog;
     private Activity context;
     String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAigAoSVGbSqLE2Dt7BjghiEZp4q8TR0m1Et3EGU6Z6h06/SBbN1w4I661Xp6JKSOB9Wy5VG/H1WtBgfA+91LQl5utnphAVxQPUhAsbjHnzAta804d5Bn6wb3FvKvby4HkdyyisjSf/vcKY4dfCD3GQEHSqGdF+JYBepJqX5xg7CVYP05TMXPjpHJvcNo6S0a2eCtLC5AFjxYp1nuP/p8vunkW5YswdIvsc9M40LeAGInR9zilgCd3xECN/KsPxyKMNLVKK26QAmTKESpyhXRFXdO9k+JEtGGcK7faeVvlCNinozhugwnKQEgk2DdfiuyXr77PQdhq2+TX5sIb16TQCQIDAQAB";
     private IabHelper mHelper;
@@ -51,7 +52,9 @@ public class AboutDialog {
 
         ab.setView(v);
 
-        ab.show();
+        dialog = ab.create();
+        dialog.show();
+
         mHelper = new IabHelper(AboutDialog.this.context, publicKey);
         mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
             public void onIabSetupFinished(IabResult result) {
@@ -69,6 +72,14 @@ public class AboutDialog {
         return context;
     }
 
+    private void onDispose()
+    {
+        if(this.mHelper != null) {
+            mHelper.dispose();
+        }
+        mHelper = null;
+    }
+
     class OkListener implements DialogInterface.OnClickListener {
 
         public void onClick(DialogInterface dialog, int which) {
@@ -76,6 +87,7 @@ public class AboutDialog {
             SharedPreferences.Editor e = pr.edit();
             e.putBoolean("aboutShown", true);
             e.commit();
+            onDispose();
         }
 
     }
@@ -94,6 +106,7 @@ public class AboutDialog {
 
             /* Send it off to the Activity-Chooser */
             getContext().startActivity(emailIntent);
+            onDispose();
         }
 
     }
@@ -102,16 +115,51 @@ public class AboutDialog {
 
         public void onClick(DialogInterface dialog, int which) {
 
+            AlertDialog.Builder ab = new AlertDialog.Builder(context);
+            ab.setTitle("Přispějte na vývoj");
+            ab.setMessage("Condroid vyvíjím již 5 let. Aby vám zpříjmňoval pobyt na conech. A je k dispozici zdarma.\n\nPodpořte prosím další vývoj aplikace zaslání příspěvku. Nejjedodušší je to přímo tady - prostě koupit pivo! Přes PayPal můžete pak zaslat libovolnou částku.");
+            ab.setCancelable(true);
+            ab.setPositiveButton("Koupit pivo!", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    mHelper.launchPurchaseFlow(AboutDialog.this.context, "large_beer", 10001,
+                            new IabHelper.OnIabPurchaseFinishedListener() {
+                                @Override
+                                public void onIabPurchaseFinished(IabResult result, Purchase info) {
+                                    if (result.isSuccess()) {
+                                        IcsToast.makeText(context, "Díky!", Toast.LENGTH_LONG).show();
+                                    }
+                                    if (result.isFailure()) {
+                                        IcsToast.makeText(context, "Příspěvek můžete zaslat kdykoli přes dialog O aplikaci.", Toast.LENGTH_LONG).show();
+                                    }
+                                    onDispose();
+                                }
+                            }, ""
+                    );
+                }
+            });
+            ab.setNeutralButton("Paypal", new DialogInterface.OnClickListener() {
 
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations" +
+                            "&business=2Z394R5DLKGU4&lc=CZ&item_name=Condroid&currency_code=CZK" +
+                            "&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest"));
+                    getContext().startActivity(intent);
+                    onDispose();
+                }
+            });
+            ab.setNegativeButton("Později", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    IcsToast.makeText(context, "Příspěvek můžete zaslat kdykoli přes dialog O aplikaci.", Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+                    onDispose();
+                }
+            });
+            ab.create().show();
 
-
-            mHelper.launchPurchaseFlow(AboutDialog.this.context, "small_beer", 10001,
-                    new IabHelper.OnIabPurchaseFinishedListener() {
-                        @Override
-                        public void onIabPurchaseFinished(IabResult result, Purchase info) {
-                            IcsToast.makeText(context, info.getSku(), Toast.LENGTH_LONG).show();
-                        }
-                    }, "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ");
 
         }
 
@@ -123,6 +171,8 @@ public class AboutDialog {
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("http://mobile.twitter.com/Condroid_CZ"));
             getContext().startActivity(intent);
+            onDispose();
+            dialog.dismiss();
         }
 
     }
