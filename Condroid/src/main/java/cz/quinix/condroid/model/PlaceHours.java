eea9101e40b87bb061493f01463f9d305b0d59b0
Jan Langer <jan.langer@gmail.com>
2014-06-24 00:02:36 +0200

    places info activity

--- Condroid/src/main/java/cz/quinix/condroid/model/PlaceHours.java
+++ Condroid/src/main/java/cz/quinix/condroid/model/PlaceHours.java
@@ -3,10 +3,14 @@ package cz.quinix.condroid.model;
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
 
 /**
  * Created by Jan on 21. 6. 2014.
@@ -17,6 +21,9 @@ public class PlaceHours implements Serializable {
     private static final int TYPE_WEEK = 2;
 
     static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    static SimpleDateFormat hourFormat = new SimpleDateFormat("HH:mm");
+    static SimpleDateFormat dayFormat = new SimpleDateFormat("dd. MM. ");
+
 
 
     public int type;
@@ -71,21 +78,7 @@ public class PlaceHours implements Serializable {
 
     private Date[] getCurrentDateHours() {
         Date now = new Date();
-        if(parsedDates == null) {
-            parsedDates = new HashMap<Integer, Date[]>();
-
-            for(int key: hours.keySet()) {
-                String[] dates = hours.get(key);
-
-                try {
-                    Date opening = dateFormat.parse(dates[0]);
-                    Date closing = dateFormat.parse(dates[1]);
-                    parsedDates.put(key, new Date[]{opening, closing});
-                } catch (ParseException e) {
-
-                }
-            }
-        }
+        parseData();
 
         for(int key: parsedDates.keySet()) {
             Date[] hours = parsedDates.get(key);
@@ -101,5 +94,92 @@ public class PlaceHours implements Serializable {
             }
         }
         return null;
+    }
+
+    private void parseData() {
+        if(parsedDates == null) {
+            parsedDates = new HashMap<Integer, Date[]>();
+
+            for(int key: hours.keySet()) {
+                String[] dates = hours.get(key);
+
+                try {
+                    Date opening = dateFormat.parse(dates[0]);
+                    Date closing = dateFormat.parse(dates[1]);
+                    parsedDates.put(key, new Date[]{opening, closing});
+                } catch (ParseException e) {
+
+                }
+            }
+        }
+    }
+
+    public String getReadableTitleFor(int key) {
+        if(this.type == TYPE_DAYS) {
+            String[] values = hours.get(key);
+
+            try {
+                Date opening = dateFormat.parse(values[0]);
+                return dayFormat.format(opening);
+            } catch (ParseException e) {
+                return "";
+            }
+        } else {
+            String[] dayOfWeek = {"","po", "út", "st", "čt", "pá", "so", "ne"};
+
+            return dayOfWeek[key];
+        }
+    }
+
+    public String[] getHoursFor(int key) {
+        if(this.type == TYPE_DAYS) {
+            String[] values = hours.get(key);
+
+            try {
+                Date opening = dateFormat.parse(values[0]);
+                Date closing = dateFormat.parse(values[1]);
+                return new String[]{hourFormat.format(opening), hourFormat.format(closing)};
+            } catch (ParseException e) {
+                return null;
+            }
+        } else {
+            return hours.get(key);
+        }
+
+    }
+
+    public boolean isToday(int key) {
+        if(type == TYPE_WEEK) {
+            Date now = new Date();
+
+            int dayOfWeek = now.getDay();
+            if (dayOfWeek == 0) {
+                dayOfWeek = 7;
+            }
+            return dayOfWeek == key;
+        } else {
+            parseData();
+            Date[] hours = parsedDates.get(key);
+            Calendar today = Calendar.getInstance(TimeZone.getDefault(), new Locale("cs", "CZ"));
+            today.setTime(new Date());
+
+            Calendar compared = Calendar.getInstance();
+            compared.setTime( hours[0]);
+
+            //its today
+            return compared.get(Calendar.YEAR) == today.get(Calendar.YEAR)
+                    && compared.get(Calendar.MONTH) == today.get(Calendar.MONTH)
+                    && compared.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH);
+        }
+    }
+
+    public Integer[] getKeys() {
+        if(type == TYPE_WEEK) {
+            return new Integer[]{1, 2, 3, 4, 5, 6, 7};
+        } else {
+            Set<Integer> i = hours.keySet();
+
+            return i.toArray(new Integer[i.size()]);
+        }
     }
 }
--- Condroid/src/main/java/cz/quinix/condroid/ui/activities/NeighbourhoodActivity.java
+++ Condroid/src/main/java/cz/quinix/condroid/ui/activities/NeighbourhoodActivity.java
@@ -1,12 +1,26 @@
 package cz.quinix.condroid.ui.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.actionbarsherlock.view.MenuItem;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Locale;
+
 import cz.quinix.condroid.R;
+import cz.quinix.condroid.model.Convention;
 import cz.quinix.condroid.model.Place;
+import cz.quinix.condroid.model.PlaceHours;
+import cz.quinix.condroid.ui.listeners.MakeFavoritedListener;
+import cz.quinix.condroid.ui.listeners.SetReminderListener;
 
 /**
  * Created by Jan on 22. 6. 2014.
@@ -24,6 +38,98 @@ public class NeighbourhoodActivity extends RoboSherlockActivity {
         this.place = (Place) this.getIntent().getSerializableExtra(
                 "place");
 
-        Toast.makeText(this, place.getName(), Toast.LENGTH_LONG).show();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView title = (TextView) findViewById(R.id.annot_title);
+        title.setText(place.getName());
+
+        TextView tAddress = (TextView) findViewById(R.id.tAddress);
+
+        if(place.getAddress() != null) {
+            tAddress.setText(StringUtils.join(place.getAddress(), "\n"));
+        }
+        else {
+            tAddress.setVisibility(View.GONE);
+        }
+
+        TextView tDescription = (TextView) findViewById(R.id.tDescription);
+
+        if(place.getDescription() != null) {
+            tDescription.setText(place.getDescription());
+        }
+        else {
+            tDescription.setVisibility(View.GONE);
+        }
+
+        TextView tUrl = (TextView) findViewById(R.id.tUrl);
+        if(place.getUrl() != null) {
+            tUrl.setText(place.getUrl());
+        }
+        else {
+            tUrl.setVisibility(View.GONE);
+        }
+
+        if(place.getGps() != null) {
+            LinearLayout header = (LinearLayout) findViewById(R.id.lPlaceHeader);
+            header.setClickable(true);
+
+            header.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(place.getGps() != null) {
+                        String uri = String.format(Locale.ENGLISH, "geo:0,0?q=%f,%f(%s)&z=19", place.getGps().lat, place.getGps().lon, place.getName());
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        this.setupOpening();
+
+    }
+
+    private void setupOpening() {
+        if(place.getHours() != null) {
+            findViewById(R.id.lMissing).setVisibility(View.GONE);
+            LinearLayout parent = (LinearLayout) findViewById(R.id.lOpeningHours);
+            PlaceHours hours = place.getHours();
+            for(int key: hours.getKeys()) {
+
+
+                View child = getLayoutInflater().inflate(R.layout.neighbourhood_activity_item, null);
+
+                TextView item = (TextView) child.findViewById(R.id.tDay);
+                item.setText(hours.getReadableTitleFor(key));
+
+                TextView item2 = (TextView) child.findViewById(R.id.tTitle);
+                String[] items = hours.getHoursFor(key);
+                if(items == null) {
+                    item2.setText(getString(R.string.lClosed));
+                } else {
+                    item2.setText(items[0] + " - " + items[1]);
+                }
+                if(hours.isToday(key)) {
+                    child.setBackgroundResource(R.color.mediumgray);
+                }
+
+                parent.addView(child);
+            }
+        } else {
+            findViewById(R.id.lMissing).setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, NeighbourhoodListActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
