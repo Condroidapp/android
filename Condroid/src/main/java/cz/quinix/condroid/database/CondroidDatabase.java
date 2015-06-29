3061c60be78f42ccdfc9039072cd0a4e2b05b384
Jan Langer <jan.langer@gmail.com>
2015-06-29 21:30:01 +0200

    changes

--- Condroid/src/main/java/cz/quinix/condroid/database/CondroidDatabase.java
+++ Condroid/src/main/java/cz/quinix/condroid/database/CondroidDatabase.java
@@ -2,11 +2,16 @@ package cz.quinix.condroid.database;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+
+import java.util.ArrayList;
 
 public class CondroidDatabase {
 
@@ -80,7 +85,7 @@ public class CondroidDatabase {
 
 	}
 
-	static class CondroidOpenHelper extends SQLiteOpenHelper {
+	public static class CondroidOpenHelper extends SQLiteOpenHelper {
 
 		private static final String DATABASE_CREATE_CONS =
 				"CREATE TABLE \"" + CON_TABLE + "\" ( " +
@@ -181,6 +186,56 @@ public class CondroidDatabase {
 					this.onCreate(db);
 				}
 			}
+		}
+
+		public ArrayList<Cursor> getData(String Query){
+			//get writable database
+			SQLiteDatabase sqlDB = this.getWritableDatabase();
+			String[] columns = new String[] { "mesage" };
+			//an array list of cursor to save two cursors one has results from the query
+			//other cursor stores error message if any errors are triggered
+			ArrayList<Cursor> alc = new ArrayList<Cursor>(2);
+			MatrixCursor Cursor2= new MatrixCursor(columns);
+			alc.add(null);
+			alc.add(null);
+
+
+			try{
+				String maxQuery = Query ;
+				//execute the query results will be save in Cursor c
+				Cursor c = sqlDB.rawQuery(maxQuery, null);
+
+
+				//add value to cursor2
+				Cursor2.addRow(new Object[] { "Success" });
+
+				alc.set(1,Cursor2);
+				if (null != c && c.getCount() > 0) {
+
+
+					alc.set(0,c);
+					c.moveToFirst();
+
+					return alc ;
+				}
+				return alc;
+			} catch(SQLException sqlEx){
+				Log.d("printing exception", sqlEx.getMessage());
+				//if any exceptions are triggered save the error message to cursor an return the arraylist
+				Cursor2.addRow(new Object[] { ""+sqlEx.getMessage() });
+				alc.set(1,Cursor2);
+				return alc;
+			} catch(Exception ex){
+
+				Log.d("printing exception", ex.getMessage());
+
+				//if any exceptions are triggered save the error message to cursor an return the arraylist
+				Cursor2.addRow(new Object[] { ""+ex.getMessage() });
+				alc.set(1,Cursor2);
+				return alc;
+			}
+
+
 		}
 
 	}
